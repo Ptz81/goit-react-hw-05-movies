3df0d81b0e5e749237292712dad54baf32e03869
Taras Papka <taras.papka@protonmail.com>
2023-05-28 23:48:55 +0300

    correction of mistakes in Moviedetatils

--- src/Service/Api.jsx
+++ src/Service/Api.jsx
@@ -1,63 +1,65 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    method: 'GET',
-    IMG: 'https://image.tmdb.org/t/p/w342',
-    baseURL: 'https://api.themoviedb.org/3',
-    params: {
-        api_key: 'c91e59fa560fc5d9b1a82432410ac72e',
-        language: 'en-US',
-    },
+  method: 'GET',
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: 'c91e59fa560fc5d9b1a82432410ac72e',
+    language: 'en-US',
+  },
 });
 
-
 export async function getTrendMovies() {
-  const routeWay = `/trending/movie/week`;
+  const routeWay = '/trending/movie/week';
   try {
     const { data } = await instance.get(routeWay);
     return data.results;
   } catch (error) {
-    throw console.error(error);
+    console.error(error);
+    throw error;
   }
 }
 
 export async function getRequestedMovie(movieId) {
-    const routeWay = `/search/${movieId}`;
+  const routeWay = `/movie/${movieId}`;
   try {
     const { data } = await instance.get(routeWay);
-    return data.results;
+    return data;
   } catch (error) {
-    throw console.error(error);
+    console.error(error);
+    throw error;
   }
 }
 
 export async function getMovieDetails(movieId) {
-    const routeWay = `/movie/${movieId}`;
+  const routeWay = `/movie/${movieId}`;
   try {
     const { data } = await instance.get(routeWay);
-    return data.results;
+    return data;
   } catch (error) {
-    throw console.error(error);
+    console.error(error);
+    throw error;
   }
 }
 
 export async function getMovieCredits(movieId) {
-    const routeWay = `/movie/${movieId}/credits`;
+  const routeWay = `/movie/${movieId}/credits`;
   try {
     const { data } = await instance.get(routeWay);
-    return data.results;
+    return data;
   } catch (error) {
-    throw console.error(error);
+    console.error(error);
+    throw error;
   }
 }
 
 export async function getMovieReview(movieId) {
-    const routeWay = `/movie/${movieId}/reviews`;
+  const routeWay = `/movie/${movieId}/reviews`;
   try {
     const { data } = await instance.get(routeWay);
-    return data.results;
+    return data;
   } catch (error) {
-    throw console.error(error);
+    console.error(error);
+    throw error;
   }
 }
-
--- src/pages/Movies/MoviesDetails.jsx
+++ src/pages/Movies/MoviesDetails.jsx
@@ -1,4 +1,108 @@
-import { useEffect, useState, Suspense } from "react";
+// import { useEffect, useState, Suspense } from "react";
+// import { Link, useLocation, useNavigate, useParams, Routes, Route } from "react-router-dom";
+// import { getMovieDetails } from '../../Service/Api';
+
+// import Cast from '../../components/Cast/Cast';
+// import Reviews from '../../components/Reviews/Reviews';
+// import css from '../../components/App.module.css';
+// import spareIMG from '../../components/img/spareIMG.png';
+
+// const IMAGEERR = 'https://image.tmdb.org/t/p/w500/';
+
+// const MoviesDetails = () => {
+//   const [film, setFilm] = useState(null);
+//   const { movieId } = useParams();
+//   const movies = JSON.parse(localStorage.getItem('movie'));
+//   const movie = movies && movies.find(elem => elem.id === movieId);
+//   const locationDetails = useLocation();
+//   const navigate = useNavigate();
+//   const handleClick = () => {
+//     navigate(locationDetails?.state?.from || '/');
+//   };
+
+//   useEffect(() => {
+//     const fetchFilmData = async () => {
+//       try {
+//         const movieData = await getMovieDetails(movieId);
+//         setFilm(movieData);
+//       } catch (e) {
+//         console.log(e);
+//       }
+//     };
+//     fetchFilmData();
+//   }, [movieId]);
+
+//   const { genres, title, overview, vote_average, poster_path } = movie || {};
+
+//   const image = poster_path ? IMAGEERR + poster_path : spareIMG;
+//   const userScore = Math.round((Number(vote_average) * 100) / 10);
+//   const movieGenres = genres && genres.map(genre => genre.name).join('');
+
+//   return (
+//     <>
+//       {film && (
+//         <>
+//           <button className="btn btn-success m2" onClick={handleClick}>
+//             {locationDetails?.state?.label || 'Go Back'}
+//           </button>
+//           <div className={css.box}>
+//             <img src={image} alt={title} />
+
+//             <div className={css.description}>
+//               <h2>{title}</h2>
+//               <p>User Score: {userScore}%</p>
+//               <h3>Overview</h3>
+//               <p>{overview}</p>
+//               <h4>Genres</h4>
+//               <p>{movieGenres}</p>
+//             </div>
+//           </div>
+
+//           <div className={css.additional}>
+//             <h5>Additional information</h5>
+//             <ul>
+//               <li>
+//                 <Link
+//                   to={`/movies/${movieId}/cast`}
+//                   state={{
+//                     from: locationDetails.state?.from || '/',
+//                     label: locationDetails.state?.label,
+//                   }}
+//                 >
+//                   Cast
+//                 </Link>
+//               </li>
+
+//               <li>
+//                 <Link
+//                   to={`/movies/${movieId}/reviews`}
+//                   state={{
+//                     from: locationDetails.state?.from || '/',
+//                     label: locationDetails.state?.label,
+//                   }}
+//                 >
+//                   Reviews
+//                 </Link>
+//               </li>
+//             </ul>
+//           </div>
+
+//           <Suspense fallback={<p>Loading...</p>}>
+//             <Routes>
+//               <Route path="/movies/:movieId/cast" element={<Cast />} />
+//               <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+//             </Routes>
+//           </Suspense>
+//         </>
+//       )}
+//     </>
+//   );
+// };
+
+// export default MoviesDetails;
+
+
+import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate, useParams, Routes, Route } from "react-router-dom";
 import { getMovieDetails } from '../../Service/Api';
 
@@ -7,15 +111,14 @@ import Reviews from '../../components/Reviews/Reviews';
 import css from '../../components/App.module.css';
 import spareIMG from '../../components/img/spareIMG.png';
 
-const IMAGEERR = 'https://image.tmdb.org/t/p/w500/';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
 
 const MoviesDetails = () => {
   const [film, setFilm] = useState(null);
   const { movieId } = useParams();
-  const movies = JSON.parse(localStorage.getItem('movie'));
-  const movie = movies && movies.find(elem => elem.id === movieId);
   const locationDetails = useLocation();
   const navigate = useNavigate();
+
   const handleClick = () => {
     navigate(locationDetails?.state?.from || '/');
   };
@@ -25,76 +128,46 @@ const MoviesDetails = () => {
       try {
         const movieData = await getMovieDetails(movieId);
         setFilm(movieData);
-      } catch (e) {
-        console.log(e);
+      } catch (error) {
+        console.log(error);
       }
     };
     fetchFilmData();
   }, [movieId]);
 
-  const { genres, title, overview, vote_average, poster_path } = movie || {};
+  if (!film) {
+    return <p>Loading...</p>;
+  }
 
-  const image = poster_path ? IMAGEERR + poster_path : spareIMG;
+  const { title, overview, vote_average, poster_path, genres } = film;
+
+  const image = poster_path ? IMAGE_URL + poster_path : spareIMG;
   const userScore = Math.round((Number(vote_average) * 100) / 10);
-  const movieGenres = genres && genres.map(genre => genre.name).join('');
+  const movieGenres = genres && genres.map(genre => genre.name).join(', ');
 
   return (
     <>
-      {film && (
-        <>
-          <button className="btn btn-success m2" onClick={handleClick}>
-            {locationDetails?.state?.label || 'Go Back'}
-          </button>
-          <div className={css.box}>
-            <img src={image} alt={title} />
+      <button className="btn btn-success m-2" onClick={handleClick}>
+        {locationDetails?.state?.label || 'Go Back'}
+      </button>
+      <div className={css.box}>
+        <img src={image} alt={title} />
 
-            <div className={css.description}>
-              <h2>{title}</h2>
-              <p>User Score: {userScore}%</p>
-              <h3>Overview</h3>
-              <p>{overview}</p>
-              <h4>Genres</h4>
-              <p>{movieGenres}</p>
-            </div>
-          </div>
-
-          <div className={css.additional}>
-            <h5>Additional information</h5>
-            <ul>
-              <li>
-                <Link
-                  to={`/movies/${movieId}/cast`}
-                  state={{
-                    from: locationDetails.state?.from || '/',
-                    label: locationDetails.state?.label,
-                  }}
-                >
-                  Cast
-                </Link>
-              </li>
-
-              <li>
-                <Link
-                  to={`/movies/${movieId}/reviews`}
-                  state={{
-                    from: locationDetails.state?.from || '/',
-                    label: locationDetails.state?.label,
-                  }}
-                >
-                  Reviews
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <Suspense fallback={<p>Loading...</p>}>
-            <Routes>
-              <Route path="/movies/:movieId/cast" element={<Cast />} />
-              <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-            </Routes>
-          </Suspense>
-        </>
-      )}
+        <div className={css.description}>
+          <h2>{title}</h2>
+          <p>User Score: {userScore}%</p>
+          <h3>Overview</h3>
+          <p>{overview}</p>
+          <h4>Genres</h4>
+          <p>{movieGenres}</p>
+          <Routes>
+            <Route path="/movies/:movieId/cast" element={<Cast />} />
+            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          </Routes>
+          <Link to={`/movies/${movieId}/cast`}>Cast</Link>
+          <Link to={`/movies/${movieId}/reviews`}>Reviews</Link>
+        </div>
+      </div>
     </>
   );
 };
