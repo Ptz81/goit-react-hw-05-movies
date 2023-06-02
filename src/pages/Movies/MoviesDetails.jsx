daa2775ae5c1ef1d92453dd8b95639551629544f
Taras Papka <taras.papka@protonmail.com>
2023-06-02 22:54:30 +0300

    corrected location state

--- src/pages/Movies/MoviesDetails.jsx
+++ src/pages/Movies/MoviesDetails.jsx
@@ -80,8 +80,10 @@ const { title, overview, vote_average, poster_path, genres } = film || {};
                     <Link
                       to={`cast`}
                   state={{
-                    from: locationDetails.state.from,
-                    label: locationDetails.state.label,
+                    // from: locationDetails.state.from,
+                    // label: locationDetails.state.label,
+ from: locationDetails?.state?.from || '/',
+    label: locationDetails?.state?.label || '',
                   }}
                   className='btn btn-outline-primary'
                 >
