f49f2d3462287276a4bb975619bfcadec34c15a3
Taras Papka <taras.papka@protonmail.com>
2023-06-02 11:49:53 +0300

    corrected path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import Header from "./Header/Header";
 
 const Home = lazy(() => import('../pages/Home/Home.jsx'));
 const Movies = lazy(() => import('../pages/Movies/Movie.jsx'));
-const MovieDetails = lazy(() => import('../pages/Movies/MoviesDetails'));
+const MoviesDetails = lazy(() => import('../pages/Movies/MoviesDetails.jsx'));
 
 export const App = () => {
   return (
@@ -16,7 +16,7 @@ export const App = () => {
         <Routes>
           <Route path="/*" element={<Home/>} />
           <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId/*" element={<MovieDetails />}/>
+          <Route path="/movies/:movieId/*" element={<MoviesDetails />}/>
         </Routes>
         </Suspense>
       </div>
